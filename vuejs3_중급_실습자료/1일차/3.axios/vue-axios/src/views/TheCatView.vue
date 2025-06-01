311b80f38d4b294939d368e2ee626b6b034369af
withme1004v <withme1004v@naver.com>
2025-06-01 13:51:36 +0900

    first commit

--- vuejs3_중급_실습자료/1일차/3.axios/vue-axios/src/views/TheCatView.vue
+++ vuejs3_중급_실습자료/1일차/3.axios/vue-axios/src/views/TheCatView.vue
@@ -1,14 +1,33 @@
 <script setup>
-import {ref} from 'vue';
+import {ref,onMounted} from 'vue';
 import axios from 'axios';
 
 const cats= ref([]);
-const randomCatImg = () => {
+
+//const VITE_RANDOM_DOG_API_URL =import.meta.env.VITE_RANDOM_DOG_API_URL;
+//const {VITE_RANDOM_DOG_API_URL,VITE_RANDOM_CAT_API_URL} =import.meta.env;
+const {VITE_RANDOM_DOG_API_URL:dog,VITE_RANDOM_CAT_API_URL:cat} = import.meta.env;
+
+/*const randomCatImg = () => {
 axios.get("https://api.thecatapi.com/v1/images/search").then(({data})=>{
 console.log(data[0]);
 cats.value.push(data[0])
 })
+}*/
+const randomCatImg = () => {
+axios.get(dog).then(({data})=>{
+console.log(data[0]);
+cats.value.push(data[0])
+})
+axios.get(cat).then(({data})=>{
+console.log(data[0]);
+cats.value.push(data[0])
+})
 }
+onMounted(() => {
+  randomCatImg();
+})
+
 </script>
 
 <template>
